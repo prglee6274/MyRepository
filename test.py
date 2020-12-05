440358414974406fc4f156d0760e235bda073d74
GwangYeol Lee <gwangyeal@gmail.com>
2020-12-06 02:32:32 +0900

    test6

--- test.py
+++ test.py
@@ -1,3 +1,70 @@
-a = 1;
-b=2;
-c=3;
+[
+    {
+        'sha': '4d96e63af75a927dbb7716c5549d53bd6b1bff5a', 
+        'node_id': 'MDY6Q29tbWl0Mjk1NjIyNzM3OjRkOTZlNjNhZjc1YTkyN2RiYjc3MTZjNTU0OWQ1M2JkNmIxYmZmNWE=', 
+        'commit': {
+            'author': {
+                'name': 'GwangYeol Lee', 
+                'email': 'gwangyeal@gmail.com', 
+                'date': '2020-12-05T17:20:18Z'
+            }, 
+            'committer': {
+                'name': 'GwangYeol Lee', 
+                'email': 'gwangyeal@gmail.com', 
+                'date': '2020-12-05T17:20:18Z'
+            }, 
+            'message': 'test3', 
+            'tree': {
+                'sha': 'e825c8493cb948f65403f4ae545a815f7e6c6217', 
+                'url': 'https://api.github.com/repos/prglee6274/MyRepository/git/trees/e825c8493cb948f65403f4ae545a815f7e6c6217'
+            }, 
+            'url': 'https://api.github.com/repos/prglee6274/MyRepository/git/commits/4d96e63af75a927dbb7716c5549d53bd6b1bff5a', 
+            'comment_count': 0, 
+            'verification': {
+                'verified': False, 
+                'reason': 'unsigned', 
+                'signature': None, 
+                'payload': None
+            }
+        }, 
+        'url': 'https://api.github.com/repos/prglee6274/MyRepository/commits/4d96e63af75a927dbb7716c5549d53bd6b1bff5a', 
+        'html_url': 'https://github.com/prglee6274/MyRepository/commit/4d96e63af75a927dbb7716c5549d53bd6b1bff5a', 
+        'comments_url': 'https://api.github.com/repos/prglee6274/MyRepository/commits/4d96e63af75a927dbb7716c5549d53bd6b1bff5a/comments', 
+        'author': {
+            'login': 'prglee6274', 
+            'id': 48249600, 
+            'node_id': 'MDQ6VXNlcjQ4MjQ5NjAw', 
+            'avatar_url': 'https://avatars2.githubusercontent.com/u/48249600?v=4', 
+            'gravatar_id': '', 'url': 'https://api.github.com/users/prglee6274', 
+            'html_url': 'https://github.com/prglee6274', 
+            'followers_url': 'https://api.github.com/users/prglee6274/followers', 
+            'following_url': 'https://api.github.com/users/prglee6274/following{/other_user}', 
+            'gists_url': 'https://api.github.com/users/prglee6274/gists{/gist_id}', 
+            'starred_url': 'https://api.github.com/users/prglee6274/starred{/owner}{/repo}', 
+            'subscriptions_url': 'https://api.github.com/users/prglee6274/subscriptions', 
+            'organizations_url': 'https://api.github.com/users/prglee6274/orgs', 
+            'repos_url': 'https://api.github.com/users/prglee6274/repos', 
+            'events_url': 'https://api.github.com/users/prglee6274/events{/privacy}', 
+            'received_events_url': 'https://api.github.com/users/prglee6274/received_events', 
+            'type': 'User', 
+            'site_admin': False
+        }, 
+        'committer': {
+            'login': 'prglee6274', 
+            'id': 48249600, 'node_id': 
+            'MDQ6VXNlcjQ4MjQ5NjAw', 
+            'avatar_url': 'https://avatars2.githubusercontent.com/u/48249600?v=4', 
+            'gravatar_id': '', 'url': 'https://api.github.com/users/prglee6274', 
+            'html_url': 'https://github.com/prglee6274', 
+            'followers_url': 'https://api.github.com/users/prglee6274/followers', 
+            'following_url': 'https://api.github.com/users/prglee6274/following{/other_user}', 
+            'gists_url': 'https://api.github.com/users/prglee6274/gists{/gist_id}', 
+            'starred_url': 'https://api.github.com/users/prglee6274/starred{/owner}{/repo}', 
+            'subscriptions_url': 'https://api.github.com/users/prglee6274/subscriptions', 
+            'organizations_url': 'https://api.github.com/users/prglee6274/orgs', 
+            'repos_url': 'https://api.github.com/users/prglee6274/repos', 
+            'events_url': 'https://api.github.com/users/prglee6274/events{/privacy}', 
+            'received_events_url': 'https://api.github.com/users/prglee6274/received_events', 
+            'type': 'User', 
+            'site_admin': False
+        }, 'parents': [{'sha': 'd26a425e3377ab7aebe57157c253b4bdefb3c710', 'url': 'https://api.github.com/repos/prglee6274/MyRepository/commits/d26a425e3377ab7aebe57157c253b4bdefb3c710', 'html_url': 'https://github.com/prglee6274/MyRepository/commit/d26a425e3377ab7aebe57157c253b4bdefb3c710'}]}]
